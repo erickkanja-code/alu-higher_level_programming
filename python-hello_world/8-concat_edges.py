f1d5d10c1df4411e615724046f69b05723a93b9e
erick <e.kanja@alustudent.com>
2025-02-26 06:45:57 -0800

    it's also a business

--- python-hello_world/8-concat_edges.py
+++ python-hello_world/8-concat_edges.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 #object-oriented programming with Python
-str = "Python is an interpreted, interactive, object-oriented programming\language that combines remarkable power with very clear syntax"
-str = str[39:66] + str[6] + str[107:111] + str[6] + str[0:7]
+str = "Python is an interpreted, interactive, object-oriented programming language that combines remarkable power with very clear syntax"
+str = str[39:67] + str[107:112] + str[0:6]
 print(str)
