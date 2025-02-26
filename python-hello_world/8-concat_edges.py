731136453e3a7a6e2fbae7101702ff739574bfeb
erick <e.kanja@alustudent.com>
2025-02-26 06:25:32 -0800

    concatenation edges

---
+++ python-hello_world/8-concat_edges.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python3
+#object-oriented programming with Python
+str = "Python is an interpreted, interactive, object-oriented programming\language that combines remarkable power with very clear syntax"
+str = str[39:66] + " " + str[107:111] + " " + str[0:7]
+print(str)
