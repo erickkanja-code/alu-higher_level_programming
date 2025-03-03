b4e0f477f382fba7c8aaba9cfbb1eba5803fc083
erick <e.kanja@alustudent.com>
2025-03-03 06:57:12 -0800

    order

--- python-import_modules/3-infinite_add.py
+++ python-import_modules/3-infinite_add.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
 from sys import argv
-arguments = argv
-sum_of_arguments = 0
-for i in arguments[1:]:
-    sum_of_arguments += int(i)
-print(sum_of_arguments)
+if __name__ == "__main__":
+    arguments = argv
+    sum_of_arguments = 0
+    for i in arguments[1:]:
+        sum_of_arguments += int(i)
+    print(sum_of_arguments)
