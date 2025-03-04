ae4e12a9c0f45c0a810c3272c5b307149fa42008
erick <e.kanja@alustudent.com>
2025-03-04 03:34:54 -0800

    Test cases fixed

--- python-data_structures/2-replace_in_list.py
+++ python-data_structures/2-replace_in_list.py
@@ -7,5 +7,5 @@ def replace_in_list(my_list, idx, element):
         elif idx < 0:
             return my_list
         else:
-            my_list[3] = element
+            my_list[idx] = element
             return my_list
