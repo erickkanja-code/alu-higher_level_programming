e67cdd601c8dac7ab5759693581ae7eda48a2167
erick <e.kanja@alustudent.com>
2025-03-04 02:59:18 -0800

    Secure access to an element

---
+++ python-data_structures/1-element_at.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def element_at(my_list, idx):
+    for element in my_list:
+        if idx < 0:
+            return 0
+        elif idx > len(my_list):
+            return "None"
+        else:
+            return my_list[idx]
+
