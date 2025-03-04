e712cca0f63dc7ab737dc6dd47fd04beb52dda27
erick <e.kanja@alustudent.com>
2025-03-04 03:01:00 -0800

    change made to fit test cases

--- python-data_structures/1-element_at.py
+++ python-data_structures/1-element_at.py
@@ -3,7 +3,7 @@ def element_at(my_list, idx):
     for element in my_list:
         if idx < 0:
             return 0
-        elif idx > len(my_list):
+        elif idx > (len(my_list) - 1):
             return "None"
         else:
             return my_list[idx]
