5aa9637ea89214f6c72748c9ef5e1dde90370f31
Erick <e.kanja@alustudent.com>
2025-03-14 03:37:53 -0700

    Area of a square

---
+++ python-classes/3-square.py
@@ -0,0 +1,18 @@
+"""This is a class that does size validation using exception errors"""
+
+
+class Square:
+    """This class uses exceptions"""
+
+    def __init__(self, size=0):
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+
+        if size < 0:
+            raise ValueError("size must be >=0")
+
+        self.__size = size
+
+    def area(self):
+        self.area = self.__size ** 2
+        return self.area
