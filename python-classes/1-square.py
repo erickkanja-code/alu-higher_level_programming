23b3654f60f8fcbb5f040f332927d818e87b1f6f
Erick <e.kanja@alustudent.com>
2025-03-14 03:08:12 -0700

    added the documentation

--- python-classes/1-square.py
+++ python-classes/1-square.py
@@ -1,4 +1,9 @@
 #!/usr/bin/python3
+
+"""This is class square that has an instance attribute size"""
+
+
 class Square:
+    """MMA fighters innit"""
     def __init__(self, size):
         self.__size = size
