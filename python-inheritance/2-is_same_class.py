4b96e88a8fa04d3b4453d4c78e216e6395d16ee7
Erick <e.kanja@alustudent.com>
2025-03-18 06:34:55 -0700

    ilhali

--- python-inheritance/2-is_same_class.py
+++ python-inheritance/2-is_same_class.py
@@ -3,8 +3,4 @@
 """This is a very tricky situational program"""
 def is_same_class(obj, a_class):
     """I don't think yunno what a bad day is"""
-
-    if isinstance(obj, a_class):
-        return "True"
-    else:
-        return "False"
+    return type(obj) == a_class
