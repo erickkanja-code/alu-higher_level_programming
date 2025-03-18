184aca97953436209a8690e4f0604c9cf99591cc
Erick <e.kanja@alustudent.com>
2025-03-18 06:25:42 -0700

    out of privilege

--- python-inheritance/2-is_same_class.py
+++ python-inheritance/2-is_same_class.py
@@ -1,5 +1,9 @@
 #!/usr/bin/python3
+
+"""This is a very tricky situational program"""
 def is_same_class(obj, a_class):
+    """I don't think yunno what a bad day is"""
+
     if isinstance(obj, a_class):
         return "True"
     else:
