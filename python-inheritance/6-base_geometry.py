c7722298ebdbe88246813c4091d24e55308d3f65
Erick <e.kanja@alustudent.com>
2025-03-18 06:51:29 -0700

    Improvement to geometry

---
+++ python-inheritance/6-base_geometry.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+
+"""What do you mean i can't afford"""
+
+class BaseGeometry:
+    """This is an empty class"""
+    def area(self):
+        raise Exception("area() is not implemented")
