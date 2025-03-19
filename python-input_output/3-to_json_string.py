acb32c99390658dbb6649d18f5279eaa192bcc99
Erick <e.kanja@alustudent.com>
2025-03-19 03:28:31 -0700

    no sunscreen

---
+++ python-input_output/3-to_json_string.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+import json
+"""This module returns jsor rep of an object"""
+def to_json_string(my_obj):
+    """let me try that module"""
+    my_obj_json = json.dumps(my_obj)
+    return(my_obj_json)
