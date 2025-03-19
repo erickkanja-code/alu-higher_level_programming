6621329b8ca12949e2f25e69302dcc6c55f5cd73
Erick <e.kanja@alustudent.com>
2025-03-19 03:22:59 -0700

    mimicing

--- python-input_output/2-append_write.py
+++ python-input_output/2-append_write.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 """Append to a file"""
 def append_write(filename="", text=""):
+    """Jack of all trades, master of none!"""
     with open(filename, "a+") as file:
         return(file.write(text))
