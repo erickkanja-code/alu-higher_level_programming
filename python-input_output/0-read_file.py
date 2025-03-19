1c439174ca55dbfed860643ecfd8e92f40df6434
Erick <e.kanja@alustudent.com>
2025-03-19 02:38:14 -0700

    nice tea

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 """This module prints out the contents of a file to stdout"""
 def read_file(filename=""):
+    """It will open the file and read it"""
     with open(f"{filename}", "r") as file:
         print(file.read(), end="")
