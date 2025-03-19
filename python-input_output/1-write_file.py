e1edcd0d906e776c5f76e9ed954af141e5ae9c92
Erick <e.kanja@alustudent.com>
2025-03-19 02:54:51 -0700

    this car of yours

--- python-input_output/1-write_file.py
+++ python-input_output/1-write_file.py
@@ -2,5 +2,6 @@
 """this module does a lot"""
 
 def write_file(filename="", text=""):
+    """I've sold a piece of equipment"""
     with open(f"{filename}", "w+") as file:
         return file.write(text)
