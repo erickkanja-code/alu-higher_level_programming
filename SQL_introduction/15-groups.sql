96f2ac2853132f0bfd774fd4f6479bd9f533bff7
Erick <e.kanja@alustudent.com>
2025-03-21 06:43:15 -0700

    wakifunga kesho

--- SQL_introduction/15-groups.sql
+++ SQL_introduction/15-groups.sql
@@ -1,2 +1,2 @@
 -- you have the potential to get there
-SELECT score, COUNT(score) as number FROM second_table GROUP BY score;
+SELECT score, COUNT(score) as number FROM second_table GROUP BY score ORDER BY score DESC;
