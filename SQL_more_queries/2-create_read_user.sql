b48d3e08c96becb53d0cd1e5338bbcdb24c63af4
Erick <e.kanja@alustudent.com>
2025-03-24 04:06:32 -0700

    away from those topics

---
+++ SQL_more_queries/2-create_read_user.sql
@@ -0,0 +1,5 @@
+-- The OG is not the same person
+
+CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
+CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_2_pwd';
+GRANT SELECT ON hbtn_0d_2.* TO 'user_0d_2'@'localhost';
